49e8034ed4793afe41565b300333f9388554602e
MervinTanZhiYong <115868196+MervinTanZhiYong@users.noreply.github.com>
2025-01-27 20:52:30 +0800

    Initial commit

---
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -0,0 +1,125 @@
+package com.example.businesscard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.businesscard.ui.theme.BusinessCardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BusinessCardTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    BusinessCard()
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun BusinessCard() {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    )
+    {
+        Image (
+            painter = painterResource(R.drawable.profilepic),
+            contentDescription = "Profile Picture",
+            contentScale = ContentScale.Crop,
+        )
+
+        Text (
+            text = "Mervin Tan Zhi Yong",
+            style = TextStyle(
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+        )
+        Text (
+            text = "Cybersecurity Consultant || Full-Stack Developer",
+            style = TextStyle(
+                fontSize = 16.sp,
+                color = Color.Black,
+                textAlign = TextAlign.Center
+            )
+        )
+
+    }
+
+    Column (
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Bottom,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            ContactInfo(icon = R.drawable.phone, text = "+65 9878 1131")
+            ContactInfo(icon = R.drawable.social, text = "@OrienST8")
+            ContactInfo(icon = R.drawable.email, text = "mervin.tan.2021@scis.smu.edu.sg")
+        }
+    }
+
+@Composable
+fun ContactInfo(icon: Int, text: String) {
+    Row(
+        modifier = Modifier.padding(vertical = 8.dp),
+        verticalAlignment = Alignment.Bottom,
+//        verticalArrangement = Arrangement.Bottom
+    ) {
+        Image(
+            painter = painterResource(id = icon), // Replace with your icon resource
+            contentDescription = "Icon",
+            modifier = Modifier
+                .size(24.dp)
+                .padding(end = 10.dp)
+        )
+            Text (text = text, fontSize = 14.sp)
+        }
+    }
+
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.  colorScheme.background
+    ) {
+        BusinessCard()
+    }
+}
